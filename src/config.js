5e31c3621556eeb4c2634c2513710a446ce75285
Gustavo Gomes Dias <treeelevengg@gmail.com>
2020-12-10 19:56:04 -0300

    Tirando a key do sendgrid

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@ global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem-vindo'; // Não é o m
 
 module.exports = {
     connectionString: 'mongodb+srv://Gustavo:Gustavo@cluster0.gxstv.mongodb.net/API-NodeJs-Balta?retryWrites=true&w=majority',
-    sendgridKey: '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', // enviar e-mail
+    sendgridKey: 'SendGridKey', // enviar e-mail
     containerConnectionString: 'TBD' //imagens
 }
